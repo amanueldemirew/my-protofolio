2f45b70470a68b6b6ebf157dc989bf7969ed7638
Amanuel Demirew <121667469+amanueldemirew@users.noreply.github.com>
2024-05-05 14:15:22 +0300

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/droplets.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Droplet in Window";
 
 const Home = ({ name, title }) => {
   return (
